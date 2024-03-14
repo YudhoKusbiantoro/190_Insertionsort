9cc5a770b2c3c200ec12337f4f4274ee393244c1
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2024-03-14 11:04:45 +0700

    Menyelesaikan prosedur

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -58,3 +58,17 @@ void insertionsort() {                           //prosedur inertionsort
 		}
 	}
 }
+
+
+void display() {                                             //prosedur display
+	cout << endl;                                            //output baris kosong
+	cout << "\n====================" << endl;                //output ke layar
+	cout << "Element Array yang telah tersusun" << endl;     //output ke layar
+	cout << "======================" << endl;                //output ke layar
+
+	for (int j = 0; j < n; j++) {                           //looping dengan j di mulai dari 0 hingga n - 1 
+		cout << arr[j] << endl;                             //output ke layar
+	}
+	cout << endl;                                           //output baris kosong
+
+}
