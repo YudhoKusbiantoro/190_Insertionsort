385e62a11b51de8a9ef3207577862261dd30555c
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2024-03-14 10:32:59 +0700

    Membuat prosedur untuk menampilkan hasil

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -7,10 +7,29 @@ int arr[20];                      //Membuat Array dengan panjang data 20
 int n;                            //Membuat variable inputan n
 
 
-void input() {
+void input() {                                                      //prosedur input
 	while (true)
 	{
+		cout << "Masukan jumlah dta pada Array : ";                 //Membuat inputan jumlah element Array
+		cin >> n;                                                   //Memanggil variable inputan n 
 
+		if (n <= 20) {                                              //Membuat kondisi n tidak lebih dari 20
+			break;
+		}
+		else
+		{
+			cout << "\nArray yang anda masukan minimal 20 elemen.\n";
+		}
+	}
+	cout << endl;                                                     //Membuat jarak per garis program
+	cout << "====================" << endl;                           //Membuat tampilan susunan data element array
+	cout << "Masukan element Array" << endl;
+	cout << "====================" << endl;
+
+	for (int i = 0; i < n; i++)                                       //Menggunakan perulangan for untuk menyimpan data pada array
+	{
+		cout << "Data ke-" << (i + 1) << ": ";                        //Memasukan atau menginputkan nilai data ke-n
+		cin >> arr[i];                                                //Menyimpan nilai data ke-n kedalam array arr
 	}
 }
    
